79aa4e2676fa2c5c72cb1885fd81050d2fb3851c
akashKarmakar02 <akash.anan03@gmail.com>
2025-05-01 19:42:54 +0530

    chore: update build script to include raylib.dll and remove unused libraylib binaries

--- build-windows.sh
+++ build-windows.sh
@@ -2,3 +2,4 @@ mkdir -p windows-build
 CGO_ENABLED=1 CC=x86_64-w64-mingw32-gcc GOOS=windows GOARCH=amd64 go build -o windows-build/open_breaker.exe -ldflags "-s -w" cmd/main.go 
 mkdir -p windows-build/assets
 cp -r assets/* windows-build/assets/
+cp raylib.dll windows-build/
