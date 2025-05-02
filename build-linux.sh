75db5d2d663b9b79ff94e1af271e45c691612e3f
akashKarmakar02 <akash.anan03@gmail.com>
2025-05-02 12:05:19 +0530

    fix: add x11 tag to build command for compatibility

--- build-linux.sh
+++ build-linux.sh
@@ -1,4 +1,4 @@
-go build -o open_breaker -ldflags="-s -w" cmd/main.go
+go build -o open_breaker -ldflags="-s -w" -tags=x11 cmd/main.go
 flatpak-builder build-dir --force-clean open_breaker.flatpak.yaml 
 flatpak build-export repo build-dir
 flatpak build-bundle repo open_breaker.flatpak com.openarchadia.open_breaker
